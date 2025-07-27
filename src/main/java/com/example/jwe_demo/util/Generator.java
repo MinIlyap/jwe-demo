67b227b7d67a52e32d76fbeca26abc7dc197146a
Ilya Prilepskij <ilyaprilepskiy1@gmail.com>
2025-07-27 10:52:22 +0300

    Add KeyPairGenerator utility class

---
+++ src/main/java/com/example/jwe_demo/util/Generator.java
@@ -0,0 +1,32 @@
+package com.example.jwe_demo.util;
+
+import com.nimbusds.jose.jwk.RSAKey;
+
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.util.UUID;
+
+public class Generator {
+
+    public static RSAKey generateRsaKey() {
+        try {
+            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+            keyPairGenerator.initialize(2048);
+            KeyPair keyPair = keyPairGenerator.generateKeyPair();
+            RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
+            RSAPrivateKey privateKey = (RSAPrivateKey) keyPair.getPrivate();
+
+            return new RSAKey.Builder(publicKey)
+                    .privateKey(privateKey)
+                    .keyID(UUID.randomUUID().toString())
+                    .build();
+
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("Failed to generate RSA key pair ", e);
+        }
+    }
+
+}
