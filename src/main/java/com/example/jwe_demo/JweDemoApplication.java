960349d6324be45a8a124e2c6dd0f8bab5fcc63f
Ilya Prilepskij <ilyaprilepskiy1@gmail.com>
2025-07-27 10:39:39 +0300

    Initial commit

---
+++ src/main/java/com/example/jwe_demo/JweDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.jwe_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JweDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JweDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jwe_demo/JweDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jwe_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JweDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
